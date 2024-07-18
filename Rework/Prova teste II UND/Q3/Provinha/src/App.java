daef6d48720798506b025ae355d2ee89a72d8b26
Geison de Oliveira <geisonyt3@gmail.com>
2024-07-17 23:22:22 -0300

    -

--- Rework/Prova teste II UND/Q3/Provinha/src/App.java
+++ Rework/Prova teste II UND/Q3/Provinha/src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<PunhadoMoedas> Deque = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        int op;
+
+        
     }
 }
