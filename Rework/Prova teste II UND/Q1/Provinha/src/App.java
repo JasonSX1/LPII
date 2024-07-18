905edf5ccb62f7979dc3962b5cc9216212025aad
Geison de Oliveira <geisonyt3@gmail.com>
2024-07-17 23:19:28 -0300

    -

--- Rework/Prova teste II UND/Q1/Provinha/src/App.java
+++ Rework/Prova teste II UND/Q1/Provinha/src/App.java
@@ -3,25 +3,29 @@ import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Stack <Integer> pilha = new Stack<Integer>();
+        Stack<Integer> pilha = new Stack<Integer>();
         Scanner input = new Scanner(System.in);
         int numero;
-        int primeiroNumero;
-        int cont = 1;
+        int primeiroNumero = 0;
+        boolean isFirstNumber = true;
+        int cont = 0;
 
         System.out.println("Insira um inteiro para adicionar à pilha (A inserção do 0 encerra a execução do programa)");
+
         while((numero = input.nextInt()) != 0){
+            if(isFirstNumber) {
+                primeiroNumero = numero;
+                isFirstNumber = false;
+            }
             pilha.push(numero);
         }
 
-        primeiroNumero = pilha.pop();
-
-        while(!pilha.empty()){
-            int contTemp = 0;
-            contTemp = pilha.pop();
-            if(primeiroNumero == contTemp)
-            cont++;
+        for(int num : pilha) {
+            if(num == primeiroNumero) {
+                cont++;
+            }
         }
+
         System.out.println("O primeiro número inserido foi: " + primeiroNumero);
         System.out.println("E ele ocorreu " + cont + " vezes na sua pilha.");
         System.out.println("Encerrando o programa.");
